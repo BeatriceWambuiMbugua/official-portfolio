ff1682df71eae92e77a01f31c13ff428684e6277
BeatriceWambuiMbugua <beatricewambuimbugua@gmail.com>
2021-12-30 10:22:31 +0300

    final commit

--- js/script.js
+++ js/script.js
@@ -5,11 +5,58 @@ $(document).ready(function () {
     } else {
       $(".navbar").removeClass("sticky");
     }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
   });
 
+  // slide up script if
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
+
+  // typing animation script
+  var typed = new Typed(".typing", {
+    strings: ["Developer", "Blogger", "Writer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: ["Developer", "Blogger", "Writer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
   // toggle menu/navbar script
 
   $(".menu-btn").click(function () {
     $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
   });
 });
